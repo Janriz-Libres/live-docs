85de7e784463ec6c7491ea88c5ab5c8edc508b25
Janriz Libres <janriz.libres@elinnovtech.com>
2024-10-19 23:26:14 +0800

    implement collaborative editor room

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -25,7 +25,7 @@ export const createDocument = async ({
     const room = await liveblocks.createRoom(roomId, {
       metadata,
       usersAccesses,
-      defaultAccesses: [],
+      defaultAccesses: ["room:write"],
     });
 
     revalidatePath("/");
@@ -33,5 +33,29 @@ export const createDocument = async ({
     return parseStringify(room);
   } catch (error) {
     console.log(`Error happened while creating a room: ${error}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    // TODO: Bring this back when we have the permissions system in place
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error("You do not have access to this document");
+    // }
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while getting a room: ${error}`);
   }
 };
